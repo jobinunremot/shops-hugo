946a4bf08295d653074c6ac40266c32d72e198a1
Karan Kumar <karankumar0334@gmail.com>
2022-08-11 03:07:10 +0500

    Hero Section made

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   content: ["./src/index.html"],
   theme: {
-    extend: {},
-    colors: {
-      'primary': '#FF4D55',
-      'secondary': '#484848'
+    extend: {
+      colors: {
+        'primary': '#FF4D55',
+        'secondary': '#484848'
+      },
     },
+
   },
   plugins: [],
 };
