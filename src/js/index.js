e34f84c0c1d931a05b0169b904dfff244390d866
Karan Kumar <karankumar0334@gmail.com>
2022-08-11 19:20:05 +0500

    Hamburger Menu open and close properly working

--- src/js/index.js
+++ src/js/index.js
@@ -15,6 +15,9 @@ function hideSearchList() {
 
 // Hamburger Menu
 const hamburgerMenuList = document.getElementById("hamburgerMenuList");
+const hamburgerBtn = document.getElementById("hamburgerBtn");
+const exploreList = document.getElementById("exploreList");
+const helpList = document.getElementById("helpList");
 
 function showNavMenu() {
     hamburgerMenuList.classList.remove("-left-full");
@@ -26,5 +29,27 @@ function hideNavMenu() {
     hamburgerMenuList.classList.remove("left-0");
 }
 
+function showExploreList() {
+    if (exploreList.classList.contains("invisible")) {
+        exploreList.classList.remove("invisible");
+    } else {
+        exploreList.classList.add("invisible");
+    }
+}
 
 
+function showHelpList() {
+    if (helpList.classList.contains("invisible")) {
+        helpList.classList.remove("invisible");
+        helpList.classList.add("bg-opacity-50");
+    } else {
+        helpList.classList.add("invisible");
+    }
+}
+
+document.onclick = function (e) {
+    if (e.target.id !== 'hamburgerMenuList' && e.target.id !== 'hamburgerBtn') {
+        if (e.target.offsetParent && e.target.offsetParent.id !== 'hamburgerMenuList')
+            hideNavMenu();
+    }
+};
