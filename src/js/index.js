7b1501d44465ff539fe60ee33009ee6109d52e9b
Karan Kumar <karankumar0334@gmail.com>
2022-08-11 18:58:55 +0500

    Hamburger Menu Working

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Search Input List Functionality
 const searchList = document.getElementById("searchList");
 const searchInputContainer = document.getElementById("searchInputContainer");
 
@@ -11,3 +12,19 @@ function hideSearchList() {
     searchList.classList.add("invisible");
     searchInputContainer.classList.remove("rounded-b-none");
 }
+
+// Hamburger Menu
+const hamburgerMenuList = document.getElementById("hamburgerMenuList");
+
+function showNavMenu() {
+    hamburgerMenuList.classList.remove("-left-full");
+    hamburgerMenuList.classList.add("left-0");
+}
+
+function hideNavMenu() {
+    hamburgerMenuList.classList.add("-left-full");
+    hamburgerMenuList.classList.remove("left-0");
+}
+
+
+
