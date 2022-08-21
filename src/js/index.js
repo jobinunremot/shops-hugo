f494b38ae7acfbc7156a086148257b27c0df7511
Karan Kumar <karankumar0334@gmail.com>
2022-08-22 00:13:04 +0500

    Maps pinpoints added via Javascript

--- src/js/index.js
+++ src/js/index.js
@@ -177,3 +177,89 @@ function ratingUpdate(ratingIndex) {
 
 
 }
+
+
+
+//PinPoints on Map
+// PinPoints for multiple locations 
+function initialise() {
+    // create object literal to store map properties
+    var myOptions = {
+        zoom: 18 // set zoom level
+        , mapTypeId: google.maps.MapTypeId.HYBRID // apply tile (options include ROADMAP, SATELLITE, HYBRID and TERRAIN)
+    };
+
+    // create map object and apply properties
+    var map = new google.maps.Map(document.getElementById("multipointsMap"), myOptions);
+
+    // create map bounds object
+    var bounds = new google.maps.LatLngBounds();
+
+    // create array containing locations
+    var locations = [
+        ['Coogee Beach', 34.1520123, , -118.3478165]
+        , ['Cronulla Beach', 34.1563689, -118.33498327],
+        ['Bondi Beach', 34.1455215, -118.3642243]
+        ,
+    ];
+
+    // loop through locations and add to map
+    for (var i = 0; i < locations.length; i++) {
+        // get current location
+        var location = locations[i];
+
+        // create map position
+        var position = new google.maps.LatLng(location[1], location[2]);
+
+        // add position to bounds
+        bounds.extend(position);
+
+        // create marker (https://developers.google.com/maps/documentation/javascript/reference#MarkerOptions)
+        var marker = new google.maps.Marker({
+            animation: google.maps.Animation.DROP
+            , icon: "http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png"
+            , map: map
+            , position: position
+            , title: location[0]
+        });
+
+        // create info window and add to marker (https://developers.google.com/maps/documentation/javascript/reference#InfoWindowOptions)
+        google.maps.event.addListener(marker, 'click', (
+            function (marker, i) {
+                return function () {
+                    var infowindow = new google.maps.InfoWindow();
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                };
+            }
+        )(marker, i));
+    };
+
+    // fit map to bounds
+    map.fitBounds(bounds);
+}
+
+// load map after page has finished loading
+function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=initialise"; // initialize method called using callback parameter
+    document.body.appendChild(script);
+}
+window.onload = loadScript;
+
+// PinPoints for a single location
+function loadMap() {
+
+    var mapOptions = {
+        center: new google.maps.LatLng(39.1978348, -94.5399957),
+        zoom: 10
+    };
+
+    var map = new google.maps.Map(document.getElementById("sample"), mapOptions);
+
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(17.088291, 78.442383),
+        map: map,
+    });
+}
